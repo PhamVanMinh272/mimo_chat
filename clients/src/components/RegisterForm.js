92c9f54298f76a1dcaeff966d91a007db650a321
phamvanminh272 <phamvanminh272@gmail.com>
2021-02-02 22:56:15 +0700

    submit form

--- clients/src/components/RegisterForm.js
+++ clients/src/components/RegisterForm.js
@@ -2,6 +2,52 @@ import React, { Component } from 'react';
 import { Form, Col, Row , Button} from 'react-bootstrap';
 
 class RegisterForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fullName: '',
+            phone: '',
+            username: '',
+            password: '',
+            confirmPassword: '',
+            isLoaded: false
+        };
+    }
+
+    register(e) {
+        console.log('hahsh');
+        e.preventDefault();
+        console.log(e.target);
+        const options = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json;charset=UTF-8'
+            },
+            body: JSON.stringify({
+                full_name: this.state.fullName,
+                phone: this.state.phone,
+                username: this.state.username,
+                password: this.state.password,
+                confirm_password: this.state.confirmPassword
+            })
+        }
+        fetch("http://0.0.0.0:5000/register", options)
+        .then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    isLoaded: true,
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: false,
+                })
+            }
+        )
+    }
+
     render() {
         return (
             <Form>
@@ -10,7 +56,12 @@ class RegisterForm extends Component {
                     Full Name
                     </Form.Label>
                     <Col sm="9">
-                    <Form.Control type="text" placeholder="Full Name" />
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Full Name" 
+                        value={this.state.fullName} 
+                        onChange={(e) => this.setState({fullName: e.target.value})}
+                    />
                     </Col>
                 </Form.Group>
 
@@ -19,16 +70,24 @@ class RegisterForm extends Component {
                     Phone
                     </Form.Label>
                     <Col sm="9">
-                    <Form.Control type="text" placeholder="Phone" />
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Phone" 
+                        onChange={(e) => this.setState({phone: e.target.value})}
+                    />
                     </Col>
                 </Form.Group>
 
                 <Form.Group as={Row} controlId="userName">
                     <Form.Label column sm="3">
-                    User Name
+                    Username
                     </Form.Label>
                     <Col sm="9">
-                    <Form.Control type="text" placeholder="User Name" />
+                    <Form.Control 
+                        type="text" 
+                        placeholder="User Name" 
+                        onChange={(e) => this.setState({username: e.target.value})}
+                    />
                     </Col>
                 </Form.Group>
 
@@ -37,7 +96,11 @@ class RegisterForm extends Component {
                     Password
                     </Form.Label>
                     <Col sm="9">
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control 
+                        type="password" 
+                        placeholder="Password" 
+                        onChange={(e) => this.setState({password: e.target.value})}
+                    />
                     </Col>
                 </Form.Group>
 
@@ -46,10 +109,18 @@ class RegisterForm extends Component {
                     Confirm Password
                     </Form.Label>
                     <Col sm="9">
-                    <Form.Control type="password" placeholder="Confirm Password" />
+                    <Form.Control 
+                        type="password" 
+                        placeholder="Confirm Password" 
+                        onChange={(e) => this.setState({confirmPassword: e.target.value})}
+                    />
                     </Col>
                 </Form.Group>
-                <Button variant="primary" type="submit">
+                <Button 
+                    variant="primary" 
+                    type="submit"
+                    onClick={(e) => this.register(e)}
+                >
                     Register
                 </Button>
             </Form>
